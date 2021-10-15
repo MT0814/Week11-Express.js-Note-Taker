5f47967521d36c996625ce795ddb39b22b05e2e7
millie_tsai <millie_tsai@me.com>
2021-10-14 18:31:01 -0700

    updated js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const path = require("path")
 const app = express();
+const fs = require('fs');
+const util = require('util');
+const { v4: uuidv4 } = require('uuid');
 
 const PORT = process.env.PORT || 3001
 
@@ -10,11 +13,56 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
+
+
 // GET Route for retrieving all the tips
-// app.get('/api/tips', (req, res) => {
-//     console.info(`${req.method} request received for tips`);
-//     readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
-//   });
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for tips`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+
+// POST Route for a new UX/UI tip
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+
+    const { title, text } = req.body
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4()
+        };
+
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
