36fe7f686bea648e53c67b65e1a24ccd1e27387d
millie_tsai <millie_tsai@me.com>
2021-10-21 15:07:25 -0700

    updated js

--- server.js
+++ server.js
@@ -68,6 +68,10 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// app.get('/*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// );
+
 app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
